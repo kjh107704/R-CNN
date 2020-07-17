70447641055475a79334af5b0d8a20c3634f4cdd
Juhui Kim <kjh107704@gmail.com>
2020-07-17 21:33:12 +0900

    feat: training 로직을 추가함 vgg16 model을 이용한 training logic을 추가했음 그러나, training dataset의 output값이 model input과 맞지 않아 오류 발생함(#1)

--- R-CNN/R-CNN.py
+++ R-CNN/R-CNN.py
@@ -124,3 +124,61 @@ Y_new = np.array(train_labels)
 # %%
 train_dataSet = AirplaneAndBackgroundDataset(X_new,Y_new)
 train_dataloader = DataLoader(dataset=dataSet,batch_size=2,shuffle=True)
+# %%
+vgg = models.vgg16(pretrained=True)
+
+for param in vgg.features.parameters():
+    param.requires_grad = False
+
+vgg.classifier[6].out_features = 2
+
+optimizer = optim.SGD(vgg.classifier.parameters(), lr=0.0001, momentum=0.5)
+# %%
+
+def fit(epoch, model, data_loader, phase='training', volatile=False):
+    is_cuda = torch.cuda.is_available()
+    optimizer = optim.Adam(model.parameters())
+    
+    if phase == 'training':
+        model.train()
+    if phase == 'validation':
+        model.eval()
+        volatile=True
+    
+    running_loss = 0.0
+    running_correct = 0
+
+    for batch_idx, (data, target) in enumerate(data_loader):
+        if is_cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile), Variable(target)
+
+        if phase == 'training':
+            optimizer.zero_grad()
+        output = model(data)
+
+        loss = F.nll_loss(output, target)
+
+        running_loss += F.nll_loss(output, target, size_average=False).data[0]
+        preds = output.data.max(dim=1, keepdim=True)
+
+        if phase == 'training':
+            loss.backward()
+            optimizer.step()
+
+        loss = running_loss/len(data_loader.dataset)
+        accuracy = 100. * running_correct/len(data_loader.dataset)
+
+        print(f'{phase} loss is {loss:{5}.{2}} and {phase} accuracy is {running_correct}/{len(data_loader.dataset)}{accuracy:{10}.{4}}')
+        return loss, accuracy
+# %%
+
+train_losses, train_accuracy = [], []
+val_losses, val_accuracy = [], []
+
+for epoch in range(1,20):
+    epoch_loss, epoch_accuracy = fit(epoch, vgg, train_dataloader, phase='training')
+    val_epoch_loss, val_epoch_accuracy = fit(epoch, model, train_dataloader, phase='training')
+
+    train_losses.append(epoch_loss)
+    train_accuracy.append(epoch_accuracy)
