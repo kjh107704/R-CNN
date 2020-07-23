0d390774d46406d35dbba8bf372de09e4ecaceb8
Juhui Kim <kjh107704@gmail.com>
2020-07-23 17:11:43 +0900

    feat: color channel별 img data를 가져올 수 있는 함수 작성 heatmap 정보 중, R channel 정보만을 사용하려고 함 확장성을 위해 color channel별 img data를 가져올 수 있는 함수를 작성함

--- Selective Search/Selective Search based on CAM result.py
+++ Selective Search/Selective Search based on CAM result.py
@@ -40,3 +40,30 @@ orig_heatmaps.append(cv2.applyColorMap(cv2.resize(lion_data, (width, height)), c
 orig_heatmaps.append(cv2.applyColorMap(cv2.resize(tiger_data, (width, height)), cv2.COLORMAP_JET))
 #result = orig_heatmaps[1] *0.7 + img + 0.5
 #cv2.imwrite(PATH+'tiger_heatmap.jpg',heatmap)
+
+# %% [markdown]
+# # color channel별 heatmap 뽑기
+
+# %%
+# orig_img에서 (R, G, B) 세 가지 채널의 정보 중 특정 채널의 정보만 남겨서 넘김
+def getChannelImage(orig_img, channel):
+    channel = channel.upper()
+    channel_img = orig_img.copy()
+    if channel == 'R':
+        channel_img[:, :, 0] = 0
+        channel_img[:, :, 1] = 0
+    elif channel == 'G':
+        channel_img[:, :, 0] = 0
+        channel_img[:, :, 2] = 0
+    elif channel == 'B':
+        channel_img[:, :, 1] = 0
+        channel_img[:, :, 2] = 0
+
+    return channel_img
+
+
+# %%
+# orig_heatmap에서 `R` 계열이 가장 중요한 feature 부분을 나타내므로 해당 정보만 사용
+heatmap = getChannelImage(orig_heatmaps[1], 'r')
+# orig_heatmap의 R 채널 데이터를 gray scale로 출력
+plt.imshow(cv2.cvtColor(heatmap, cv2.COLOR_BGR2GRAY), cmap='gray')
