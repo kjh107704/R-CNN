3e9af78e39e0f3b1e82b9de2dad1fe1b1bd152f0
Juhui Kim <kjh107704@gmail.com>
2020-07-24 21:27:52 +0900

    feat: Selective Search 결과에서 후보군 얻는 로직 작성 Selective Search의 모든 bounding box들과 CAM의 bounding box를 비교하여 iou가 0.7 이상인 SS bounding box를 R-CNN의 최종 후보 region으로 선택함

--- Selective Search/Selective Search based on CAM result.py
+++ Selective Search/Selective Search based on CAM result.py
@@ -150,11 +150,100 @@ def get_image_of_compare_contour_and_bounding_box(img_binary, img):
 
 # %%
 # contour 영역과 bounding box 비교
-compareContourAndBoundingBox(gray_map, img)
+# %% [markdown]
+# # Selective Search 결과와 CAM Bounding Box 비교
+
+# %%
+def cv2_selective_search(img, searchMethod='f'):
+    ss = cv2.ximgproc.segmentation.createSelectiveSearchSegmentation()
+    ss.setBaseImage(img)
+    
+    if searchMethod == 'f':
+        ss.switchToSelectiveSearchFast()
+    elif searchMethod == 'q':
+        ss.switchToSelectiveSearchQuality()
+        
+    regions = ss.process()
+    
+    return regions
 
 
 # %%
-bounding_box = getBoundingBox(gray_map)
+SS_BB = cv2_selective_search(img)
+
+print(f'num of all regions by ss: {len(SS_BB)}')
+plt.imshow(draw_bounding_box(SS_BB, img))
+
+
+# %%
+def get_iou(_bb1, _bb2, changeScale = False):
+    if changeScale:
+        # _bb1, _bb2 = [x, y, w, h]
+        if len(_bb1) == 4 and len(_bb2) == 4:
+            bb1 = {'x1':_bb1[0], 'y1':_bb1[1], 'x2':_bb1[0]+_bb1[2], 'y2':_bb1[1]+_bb1[3]}
+            bb2 = {'x1':_bb2[0], 'y1':_bb2[1], 'x2':_bb2[0]+_bb2[2], 'y2':_bb2[1]+_bb2[3]}
+        else:
+            exit(0)
+    else:
+        # _bb1, _bb2 = ['x1':x1, 'x2':x2, 'y1':y1, 'y2':y2]
+        bb1 = _bb1
+        bb2 = _bb2
+    
+    
+    assert bb1['x1'] < bb1['x2']
+    assert bb1['y1'] < bb1['y2']
+    assert bb2['x1'] < bb2['x2']
+    assert bb2['y1'] < bb2['y2']
+
+    # determine the coordinates of the intersection rectangle
+    x_left = max(bb1['x1'], bb2['x1'])
+    y_top = max(bb1['y1'], bb2['y1'])
+    x_right = min(bb1['x2'], bb2['x2'])
+    y_bottom = min(bb1['y2'], bb2['y2'])
+
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+
+    # The intersection of two axis-aligned bounding boxes is always an
+    # axis-aligned bounding box
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+
+    # compute the area of both AABBs
+    bb1_area = (bb1['x2'] - bb1['x1']) * (bb1['y2'] - bb1['y1'])
+    bb2_area = (bb2['x2'] - bb2['x1']) * (bb2['y2'] - bb2['y1'])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
+
+
+# %%
+def get_candidate_bounding_box(SS_BB, CAM_BB):
+    # SS_BB dim = 2
+    # CAM_BB dim = 3
+    
+    bounding_box = []
+
+    for ss_bb in SS_BB:
+        for cam_bbs in CAM_BB:
+            for cam_bb in cam_bbs:
+                iou = get_iou(ss_bb, cam_bb, changeScale = True)
+                if iou > 0.7:
+                    bounding_box.append(ss_bb)
+    return bounding_box
+
+
+# %%
+bounding_box = get_candidate_bounding_box(SS_BB, CAM_BB)
+
+
+# %%
+plt.imshow(draw_bounding_box(bounding_box, img))
+print(f'num of candidate bounding box: {len(bounding_box)}')
 
 drawBoundingBox(bounding_box, img)
 
